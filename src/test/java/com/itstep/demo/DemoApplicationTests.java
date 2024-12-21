64af1ad5ca81385368c54dc3008c018062efd2d8
yuliiapnivchuk <yuliyapnivchuk@gmail.com>
2024-12-21 17:25:50 +0100

    init commit

---
+++ src/test/java/com/itstep/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstep.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
