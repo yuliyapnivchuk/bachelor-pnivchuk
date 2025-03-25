7c79cf28f60dd83e5029517d81d9649655256984
yuliiapnivchuk <yuliyapnivchuk@gmail.com>
2025-03-25 22:56:51 +0100

    fix

--- src/main/java/com/itstep/repository/ExpenseRepository.java
+++ src/main/java/com/itstep/repository/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package com.itstep.repository;
 
 import com.itstep.entity.Expense;
+import com.itstep.entity.ExpenseItemProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
